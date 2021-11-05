66e2dbbc5a856755256263f34af4c39a13592548
Neng Shi <shineng@zju.edu.cn>
2021-11-04 21:34:01 -0400

    reorganize files

--- gui/server/app.py
+++ gui/server/app.py
@@ -1,6 +1,11 @@
 from flask import Flask, jsonify,request
 from flask_cors import CORS
-
+import os
+import numpy as np
+import random
+import argparse
+import torch
+import interaction_editsql
 
 # configuration
 DEBUG = True
@@ -22,7 +27,204 @@ def ping_pong():
 def passSent():
     sentence = request.get_json()['sentence']
     print(sentence)
+    interaction_editsql.real_user_interaction(example, user, agent, eval_maximum_sql_length)
     return jsonify('yes!!!')
 
+def interpret_args():
+    """ Interprets the command line arguments, and returns a dictionary. """
+    parser = argparse.ArgumentParser()
+
+    ### Data parameters
+    parser.add_argument(
+        '--raw_train_filename',
+        type=str,
+        default='../atis_data/data/resplit/processed/train_with_tables.pkl')
+    parser.add_argument(
+        '--raw_dev_filename',
+        type=str,
+        default='../atis_data/data/resplit/processed/dev_with_tables.pkl')
+    parser.add_argument(
+        '--raw_validation_filename',
+        type=str,
+        default='../atis_data/data/resplit/processed/valid_with_tables.pkl')
+    parser.add_argument(
+        '--raw_test_filename',
+        type=str,
+        default='../atis_data/data/resplit/processed/test_with_tables.pkl')
+
+    parser.add_argument('--data_directory', type=str, default='processed_data')
+
+    parser.add_argument('--processed_train_filename', type=str, default='train.pkl')
+    parser.add_argument('--processed_dev_filename', type=str, default='dev.pkl')
+    parser.add_argument('--processed_validation_filename', type=str, default='validation.pkl')
+    parser.add_argument('--processed_test_filename', type=str, default='test.pkl')
+
+    parser.add_argument('--database_schema_filename', type=str, default=None)
+    parser.add_argument('--embedding_filename', type=str, default=None)
+
+    parser.add_argument('--input_vocabulary_filename', type=str, default='input_vocabulary.pkl')
+    parser.add_argument('--output_vocabulary_filename',
+                        type=str,
+                        default='output_vocabulary.pkl')
+
+    parser.add_argument('--input_key', type=str, default='nl_with_dates')
+
+    parser.add_argument('--anonymize', type=bool, default=False)
+    parser.add_argument('--anonymization_scoring', type=bool, default=False)
+    parser.add_argument('--use_snippets', type=bool, default=False)
+
+    parser.add_argument('--use_previous_query', type=bool, default=False)
+    parser.add_argument('--maximum_queries', type=int, default=1)
+    parser.add_argument('--use_copy_switch', type=bool, default=False)
+    parser.add_argument('--use_query_attention', type=bool, default=False)
+
+    parser.add_argument('--use_utterance_attention', type=bool, default=False)
+
+    parser.add_argument('--freeze', type=bool, default=False)
+    parser.add_argument('--scheduler', type=bool, default=False)
+
+    parser.add_argument('--use_bert', type=bool, default=False)
+    parser.add_argument("--bert_type_abb", type=str, help="Type of BERT model to load. e.g.) uS, uL, cS, cL, and mcS")
+    parser.add_argument("--bert_input_version", type=str, default='v1')
+    parser.add_argument('--fine_tune_bert', type=bool, default=False)
+    parser.add_argument('--lr_bert', default=1e-5, type=float, help='BERT model learning rate.')
+
+    ### Debugging/logging parameters
+    parser.add_argument('--logdir', type=str, default='logs')
+    parser.add_argument('--deterministic', type=bool, default=False)
+    parser.add_argument('--num_train', type=int, default=-1)
+
+    parser.add_argument('--logfile', type=str, default='log.txt')
+    parser.add_argument('--results_file', type=str, default='results.txt')
+
+    ### Model architecture
+    parser.add_argument('--input_embedding_size', type=int, default=300)
+    parser.add_argument('--output_embedding_size', type=int, default=300)
+
+    parser.add_argument('--encoder_state_size', type=int, default=300)
+    parser.add_argument('--decoder_state_size', type=int, default=300)
+
+    parser.add_argument('--encoder_num_layers', type=int, default=1)
+    parser.add_argument('--decoder_num_layers', type=int, default=2)
+    parser.add_argument('--snippet_num_layers', type=int, default=1)
+
+    parser.add_argument('--maximum_utterances', type=int, default=5)
+    parser.add_argument('--state_positional_embeddings', type=bool, default=False)
+    parser.add_argument('--positional_embedding_size', type=int, default=50)
+
+    parser.add_argument('--snippet_age_embedding', type=bool, default=False)
+    parser.add_argument('--snippet_age_embedding_size', type=int, default=64)
+    parser.add_argument('--max_snippet_age_embedding', type=int, default=4)
+    parser.add_argument('--previous_decoder_snippet_encoding', type=bool, default=False)
+
+    parser.add_argument('--discourse_level_lstm', type=bool, default=False)
+
+    parser.add_argument('--use_schema_attention', type=bool, default=False)
+    parser.add_argument('--use_encoder_attention', type=bool, default=False)
+
+    parser.add_argument('--use_schema_encoder', type=bool, default=False)
+    parser.add_argument('--use_schema_self_attention', type=bool, default=False)
+    parser.add_argument('--use_schema_encoder_2', type=bool, default=False)
+
+    ### Training parameters
+    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument('--train_maximum_sql_length', type=int, default=200)
+    parser.add_argument('--train_evaluation_size', type=int, default=100)
+
+    parser.add_argument('--dropout_amount', type=float, default=0.5)
+
+    parser.add_argument('--initial_patience', type=float, default=10.)
+    parser.add_argument('--patience_ratio', type=float, default=1.01)
+
+    parser.add_argument('--initial_learning_rate', type=float, default=0.001)
+    parser.add_argument('--learning_rate_ratio', type=float, default=0.8)
+
+    parser.add_argument('--interaction_level', type=bool, default=True)
+    parser.add_argument('--reweight_batch', type=bool, default=False)
+
+    ### Setting
+    # parser.add_argument('--train', type=bool, default=False)
+    parser.add_argument('--train', type=int, choices=[0,1], default=0)
+    parser.add_argument('--debug', type=bool, default=False)
+
+    parser.add_argument('--evaluate', type=bool, default=False)
+    parser.add_argument('--attention', type=bool, default=False)
+    parser.add_argument('--enable_testing', type=bool, default=False)
+    parser.add_argument('--use_predicted_queries', type=bool, default=False)
+    parser.add_argument('--evaluate_split', type=str, default='dev')
+    parser.add_argument('--evaluate_with_gold_forcing', type=bool, default=False)
+    parser.add_argument('--eval_maximum_sql_length', type=int, default=1000)
+    parser.add_argument('--results_note', type=str, default='')
+    parser.add_argument('--compute_metrics', type=bool, default=False)
+
+    parser.add_argument('--reference_results', type=str, default='')
+
+    parser.add_argument('--interactive', type=bool, default=False)
+
+    parser.add_argument('--database_username', type=str, default="aviarmy")
+    parser.add_argument('--database_password', type=str, default="aviarmy")
+    parser.add_argument('--database_timeout', type=int, default=2)
+
+    # interaction params - Ziyu
+    parser.add_argument('--job', default='test_w_interaction', choices=['test_w_interaction', 'online_learning'],
+                        help='Set the job. For parser pretraining, see other scripts.')
+    parser.add_argument('--seed', type=int, default=0, help='Random seed.')
+    parser.add_argument('--raw_data_directory', type=str, help='The data directory of the raw spider data.')
+    parser.add_argument('--num_options', type=str, default='3', help='[INTERACTION] Number of options.')
+    parser.add_argument('--err_detector', type=str, default='any',
+                        help='[INTERACTION] The error detector: '
+                             '(1) prob=x for using policy probability threshold;'
+                             '(2) stddev=x for using Bayesian dropout threshold (need to set --dropout and --passes);'
+                             '(3) any for querying about every policy action;'
+                             '(4) perfect for using a simulated perfect detector.')
+    parser.add_argument('--dropout', type=float, default=0.0,
+                        help='[INTERACTION] Dropout rate for Bayesian dropout-based uncertainty analysis. '
+                             'This does NOT change the dropout rate in training.')
+    parser.add_argument('--passes', type=int, default=1,
+                        help='[INTERACTION] Number of decoding passes for Bayesian dropout-based uncertainty analysis.')
+    parser.add_argument('--friendly_agent', type=int, default=0, choices=[0, 1],
+                        help='[INTERACTION] If 1, the agent will not trigger further interactions '
+                             'if any wrong decision is not resolved during parsing.')
+    parser.add_argument('--ask_structure', type=int, default=0, choices=[0, 1],
+                        help='[INTERACTION] Set to True to allow questions about query structure '
+                             '(WHERE/GROUP_COL, ORDER/HAV_AGG_v2) in NL.')
+
+    # online learning
+    parser.add_argument('--setting', type=str, default='', choices=['online_pretrain_10p', 'full_train'],
+                        help='Model setting; checkpoints will be loaded accordingly.')
+    parser.add_argument('--supervision', type=str, default='full_expert',
+                        choices=['full_expert', 'misp_neil', 'misp_neil_perfect', 'misp_neil_pos',
+                                 'bin_feedback', 'bin_feedback_expert',
+                                 'self_train', 'self_train_0.5'],
+                        help='[LEARNING] Online learning supervision based on different algorithms.')
+    parser.add_argument('--data_seed', type=int, choices=[0, 10, 100],
+                        help='[LEARNING] Seed for online learning data.')
+    parser.add_argument('--start_iter', type=int, default=0, help='[LEARNING] Starting iteration in online learing.')
+    parser.add_argument('--end_iter', type=int, default=-1, help='[LEARNING] Ending iteration in online learing.')
+    parser.add_argument('--update_iter', type=int, default=1000,
+                        help='[LEARNING] Number of iterations per parser update.')
+
+    args = parser.parse_args()
+
+    if not os.path.exists(args.logdir):
+        os.makedirs(args.logdir)
+
+    if not (args.train or args.evaluate or args.interactive or args.attention):
+        raise ValueError('You need to be training or evaluating')
+    if args.enable_testing and not args.evaluate:
+        raise ValueError('You should evaluate the model if enabling testing')
+
+    # Seeds for random number generation
+    print("## seed: %d" % args.seed)
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(args.seed)
+
+    return args
+
 if __name__ == '__main__':
-    app.run()
+    example, user, agent, eval_maximum_sql_length = interaction_editsql.main(interpret_args())
+    interaction_editsql.real_user_interaction(example, user, agent, eval_maximum_sql_length)
+    # app.run()
